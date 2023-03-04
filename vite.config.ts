c332016381b8b23d1db26fcefcb41c01a3ffe341
bengaudry <bengaudry@outlook.fr>
2023-03-04 19:56:54 +0100

    Lets try this

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import * as path from "path";
+import alias from "@rollup/plugin-alias";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  base: "./"
-})
+  plugins: [
+    svelte(),
+    alias({
+      entries: [
+        {
+          find: "@",
+          replacement: path.resolve(path.resolve(__dirname), "src"),
+        },
+      ],
+    }),
+  ],
+  base: "./",
+  build: {
+    outDir: "dist",
+  },
+});
