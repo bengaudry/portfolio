734bf30d239de113fafc3b7cb70aa768dc367858
bengaudry <bengaudry@outlook.fr>
2023-03-04 18:08:31 +0100

    Upgrades on hero section

--- src/lib/Buttons/Cta.svelte
+++ src/lib/Buttons/Cta.svelte
@@ -1,3 +1,31 @@
-<script lang="ts"></script>
+<script lang="ts">
+  export let customStyle = undefined;
+  export let content: string;
 
-<a href="#top" class="cta">Discover</a>
+  export let direction: "bottom" | "external" | "right" | undefined = undefined;
+</script>
+
+<a href="#top" class="cta" style="{customStyle}">
+  <span>{content}</span>
+  {#if direction === "bottom"} 
+  <img src="/public/arrow.svg" alt="↓" class="arrow-bottom">
+  {:else if direction === "external"}
+  <img src="/public/arrow.svg" alt="↗" class="arrow-external">
+  {:else}
+  <img src="/public/arrow.svg" alt="→" class="arrow-right">
+  {/if}
+</a>
+
+<style>
+  .arrow-external {
+    transform: rotate(-135deg);
+  }
+
+  .arrow-right {
+    transform: rotate(-90deg);
+  }
+
+  .arrow-bottom {
+    transform: rotate(0);
+  }
+</style>
--- src/lib/Sections/Hero/Hero.svelte
+++ src/lib/Sections/Hero/Hero.svelte
@@ -9,7 +9,25 @@
       <br />
       a junior dev
     </h1>
-    <p class="hero-characteristics">Based in France | 17 years old | Front-end dev </p>
-    <Cta />
+    <p class="hero-characteristics secondary-text">Based in France | 17 years old | Front-end dev </p>
+    <Cta content="Discover" customStyle="margin: 0 auto; margin-top: 2rem;" direction="bottom"/>
   </div>
 </section>
+
+<style lang="scss">
+  .hero {
+    text-align: center;
+    padding: 150px 0;
+  }
+
+  .hero-main-sentence {
+    font-size: 4.375rem;
+    font-weight: 600;
+    margin-bottom: 15px;
+  }
+
+  .hero-characteristics {
+    font-size: 1.25rem;
+    font-weight: 600;
+  }
+</style>
