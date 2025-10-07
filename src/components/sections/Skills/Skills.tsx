bf7df3953ddfbb98c64325d53108f7555acf955b
bengaudry <bengaudry@outlook.fr>
2025-10-07 10:51:21 +0200

    Add vercel to skills

--- src/components/sections/Skills/Skills.tsx
+++ src/components/sections/Skills/Skills.tsx
@@ -42,6 +42,7 @@ export function Skills() {
             { title: "VS Code" },
             { title: "Github" },
             { title: "Notion" },
+            { title: "Vercel" },
           ]}
         />
         <List
