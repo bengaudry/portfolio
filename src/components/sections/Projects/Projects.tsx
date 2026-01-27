ae51227980c04a0a249591ef6e5342062ab8420b
bengaudry <bengaudry@outlook.fr>
2026-01-27 11:58:16 +0100

    Better design for projects

--- src/components/sections/Projects/Projects.tsx
+++ src/components/sections/Projects/Projects.tsx
@@ -20,28 +20,45 @@ function Project({
   title,
   description,
   imgSrc,
+  iconSrc,
   state,
   link,
+  repoUrl
 }: {
   title: string;
   description: string;
   imgSrc: string;
+  iconSrc?: string;
   state: ProjectState;
   link?: string;
+  repoUrl?: string;
 }) {
   return (
     <List className={styles.ProjectItem} title={title}>
       <article>
-        <img
-          src={imgSrc}
-          className={styles.ProjectPicture}
-          onClick={() => {
-            if (link) open(link, "_blank");
-          }}
-        />
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          <img
+            src={imgSrc}
+            className={styles.ProjectPicture} alt={`${title} screenshot`}
+          />
+        </a>
+
         <div>
+          {iconSrc && <img src={iconSrc} alt="Icon" width={32} height={32} className={styles.ProjectIcon} />}
           <p>{description}</p>
-          <ProjectStatePill state={state} />
+          <div className={styles.ProjectLinks}>
+            <ProjectStatePill state={state} />
+            {repoUrl &&
+              <a href={repoUrl} target="_blank" rel="noopener noreferrer">
+                <img
+                  height={24}
+                  width={24}
+                  alt="Github repository"
+                  src="https://img.icons8.com/?size=100&id=62856&format=png&color=ffffff"
+                />
+              </a>
+            }
+          </div>
         </div>
       </article>
     </List>
@@ -60,20 +77,17 @@ export function Projects() {
           state="active"
           description={t("projects.split-tabs.description")}
           imgSrc="https://addons.mozilla.org/user-media/previews/full/322/322418.png?modified=1748884796"
+          iconSrc="/portfolio/split-tabs-icon.png"
           link="https://addons.mozilla.org/firefox/addon/split-tabs"
+          repoUrl="https://github.com/bengaudry/split-tabs"
         />
         <Project
           title="Studyswipe"
           state="active"
           description={t("projects.studyswipe.description")}
           imgSrc="/portfolio/studyswipe-screenshot.png"
+          iconSrc="/portfolio/studyswipe-icon.png"
           link="https://studyswipe.vercel.app"
-        />
-        <Project
-          title="Onboarder"
-          state="development"
-          description={t("projects.onboarder.description")}
-          imgSrc="/portfolio/studyswipe-screenshot.png"
         />
       </ResponsiveWrapper>
     </section>
