a035987527a238e3abc21509ecdd4446959cb6a4
bengaudry <bengaudry@outlook.fr>
2026-02-05 16:09:55 +0100

    feat: resume download page

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,7 +1,7 @@
 import styles from "./App.module.scss";
 import { About, Contact, Footer, Hero, Projects, Skills } from "../components/sections";
 import { Education } from "../components/sections/Education/Education";
-import "./styles/App.scss";
+import "../styles/App.scss";
 
 export default function App() {
   return (
