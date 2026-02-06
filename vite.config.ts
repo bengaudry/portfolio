879fedad59629801eaa24f7ecbf2a161f7872189
bengaudry <bengaudry@outlook.fr>
2026-02-06 17:37:03 +0100

    fix: removed optimizations breaking prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
 import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 import { reactRouter } from "@react-router/dev/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [reactRouter()],
-	build: {
-		rollupOptions: {
-			output: {
-				manualChunks: {
-					vendor: ["react", "react-dom"],
-					i18n: ["i18next", "react-i18next"],
-					motion: ["framer-motion"]
-				}
-			}
-		}
-	}
+	plugins: [react(), reactRouter()]
 })
