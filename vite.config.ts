33c4761ee66df4f0148017efbd1e8a9d336675a3
bengaudry <bengaudry@outlook.fr>
2026-02-06 13:15:07 +0100

    fix: router issue with netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 
 // https://vite.dev/config/
 export default defineConfig({
 	base: "https://bengaudry.dev/",
-	plugins: [react()],
+	plugins: [react(), netlifyReactRouter()],
 });
